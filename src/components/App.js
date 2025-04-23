f164e9e606328aa2a9b23b982c2836c26cd8bf7b
Bandi Kanti Chaitanya <bandikantichaitanya2@gmail.com>
2025-04-23 14:28:11 +0530

    Updated Version

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-
 import React from "react";
 import './../styles/App.css';
+import TodoList from "./TodoList";
+import { useState } from "react";
 
 const App = () => {
+
+let todos=['Learn React','Build a React app','Deploy the React app']
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+        <TodoList  todos={todos} />
     </div>
   )
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState } from 'react';
+
+function TodoList(props) {
+    let list=props.todos
+    const [completed, setCompleted] = useState([]);
+   function handleComplete(index){
+        setCompleted([...completed,index])
+    }
+  return (
+    <div>
+        <h2>Child Component</h2>
+        <ul>
+        {
+                    list.map((todo, index) => (
+                        <li key={index}>
+                            {todo}
+                            {
+                                !completed.includes(index) && (
+                                    <button onClick={()=>{handleComplete(index)}}  >Complete</button>
+                                )
+                            }
+                        </li>
+                    ))
+                }
+            </ul>
+    </div>
+  )
+}
+
+export default TodoList
